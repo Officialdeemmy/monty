45ca932ecbe7942c63e2296fcc7503e0bf1a7b64
Officialdeemmy <139757003+Officialdeemmy@users.noreply.github.com>
2023-12-12 15:06:45 +0100

    Update monty.h

--- monty.h
+++ monty.h
@@ -9,6 +9,10 @@
 #include <string.h>
 #include <ctype.h>
 
-
+void *_calloc_(unsigned int n, unsigned int size);
+void *_realloc_(void *ptr, unsigned int old_byte, unsigned int new_byte);
+stack_t *add_dnode_end(stack_t **head, const int node);
+stack_t *add_dnode(stack_t **head, const int node);
+void free_dlist(stack_t *head);
 
 #endif /*MONTY_H*/
