edcd90e623c3b66fd83a88069b0f5ae806257c81
Officialdeemmy <139757003+Officialdeemmy@users.noreply.github.com>
2023-12-12 16:07:27 +0100

    Update monty.h

--- monty.h
+++ monty.h
@@ -13,6 +13,16 @@ void *_calloc_(unsigned int n, unsigned int size);
 void *_realloc_(void *ptr, unsigned int old_byte, unsigned int new_byte);
 stack_t *add_dnode_end(stack_t **head, const int node);
 stack_t *add_dnode(stack_t **head, const int node);
-void free_dlist(stack_t *head);
+void free_dlist(stack_t *head);void (*get_opcodes(char *opc))(stack_t **stack, unsigned int line_number);
+void _push_(stack_t **head, unsigned int c_line);
+void _pall_(stack_t **head, unsigned int c_line);
+void _pint_(stack_t **head, unsigned int c_line);
+void _pop_(stack_t **head, unsigned int c_line);
+void _swap_(stack_t **head, unsigned int c_line);
+void _queue_(stack_t **head, unsigned int c_line);
+void _stack_(stack_t **head, unsigned int c_line);
+void _add_(stack_t **head, unsigned int c_line);
+void _nop_(stack_t **head, unsigned int c_line);
+void _sub_(stack_t **head, unsigned int c_line);
 
 #endif /*MONTY_H*/
