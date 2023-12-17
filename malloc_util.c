7d209b58a9430a187a7f2ff23f03e567c05cb5e5
Officialdeemmy <139757003+Officialdeemmy@users.noreply.github.com>
2023-12-17 10:55:20 +0100

    Update malloc_util.c

--- malloc_util.c
+++ malloc_util.c
@@ -32,7 +32,7 @@ void *_calloc(unsigned int n, unsigned int size)
  * @old_size: old number of bytes
  * @new_size: new number of Bytes
  */
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+void *_realloc(char *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *a = NULL;
 	unsigned int i;
