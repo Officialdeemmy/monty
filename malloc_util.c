68bd2a307ecefb4d11ca36c7be164c8ca6878c92
Officialdeemmy <139757003+Officialdeemmy@users.noreply.github.com>
2023-12-17 10:21:23 +0100

    Update malloc_util.c

--- malloc_util.c
+++ malloc_util.c
@@ -29,43 +29,43 @@ void *_calloc(unsigned int n, unsigned int size)
 /**
  * _realloc - change the size and copy the content
  * @ptr: malloc pointer to reallocate
- * @old_byte: old number of bytes
- * @new_byte: new number of Bytes
+ * @old_size: old number of bytes
+ * @new_size: new number of Bytes
  */
-void *_realloc(void *ptr, unsigned int old_byte, unsigned int new_byte)
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *a = NULL;
 	unsigned int i;
 
-	if (new_byte == old_byte)
+	if (new_size == old_size)
 		return (ptr);
 	if (ptr == NULL)
 	{
-		a = malloc(new_byte);
+		a = malloc(new_size);
 		if (!a)
 			return (NULL);
 		return (a);
 	}
-	if (new_byte == 0 && ptr != NULL)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	if (new_byte > old_byte)
+	if (new_size > old_size)
 	{
-		a = malloc(new_byte);
+		a = malloc(new_size);
 		if (!a)
 			return (NULL);
-		for (i = 0; i < old_byte; i++)
+		for (i = 0; i < old_size; i++)
 			a[i] = *((char *)ptr + i);
 		free(ptr);
 	}
 	else
 	{
-		a = malloc(new_byte);
+		a = malloc(new_size);
 		if (!a)
 			return (NULL);
-		for (i = 0; i < new_byte; i++)
+		for (i = 0; i < new_size; i++)
 			a[i] = *((char *)ptr + i);
 		free(ptr);
 	}
