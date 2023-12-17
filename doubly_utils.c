819471a308d0abfc0ac9b584810cede48c17a457
Officialdeemmy <139757003+Officialdeemmy@users.noreply.github.com>
2023-12-17 09:52:56 +0100

    Update doubly_utils.c

--- doubly_utils.c
+++ doubly_utils.c
@@ -22,3 +22,20 @@ void addnode(stack_t **head, int n)
 	new_node->prev = NULL;
 	*head = new_node;
 }
+
+/**
+* free_stack - frees a doubly linked list
+* @head: head of the stack
+*/
+void free_stack(stack_t *head)
+{
+	stack_t *aux;
+
+	aux = head;
+	while (head)
+	{
+		aux = head->next;
+		free(head);
+		head = aux;
+	}
+}
