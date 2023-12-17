9fbcd23b8f720591fcc080cd8e1542da9f22ffc3
Officialdeemmy <139757003+Officialdeemmy@users.noreply.github.com>
2023-12-17 10:03:52 +0100

    Update monty.h

--- monty.h
+++ monty.h
@@ -3,12 +3,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <sys/types.h>
-#include <sys/stat.h>
+#include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
 #include <ctype.h>
-
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -16,74 +15,67 @@
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
+/**
+ * struct bus_s - variables -args, file, line content
+ * @arg: value
+ * @file: pointer to monty file
+ * @content: line content
+ * @lifi: flag change stack <-> queue
+ * Description: carries values through the program
+ */
+typedef struct bus_s
+{
+	char *arg;
+	FILE *file;
+	char *content;
+	int lifi;
+}  bus_t;
 
+extern bus_t bus;
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
-
-/**
- * struct global_s - global structure to use in the functions
- * @lifo: is stack or queue
- * @cont: current line
- * @arg: second parameter inside the current line
- * @head: doubly linked list
- * @fd: file descriptor
- * @buffer: input text
- *
- * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO Holberton project
- */
-typedef struct global_s
-{
-	int lifo;
-	unsigned int cont;
-	char  *arg;
-	stack_t *head;
-	FILE *fd;
-	char *buffer;
-} global_t;
-
-extern global_t vglo;
-
-void *_calloc_(unsigned int n, unsigned int size);
-void *_realloc_(void *ptr, unsigned int old_byte, unsigned int new_byte);
-stack_t *add_dnode_end(stack_t **head, const int node);
-stack_t *add_dnode(stack_t **head, const int node);
-void free_dlist(stack_t *head);
-void (*get_opcode(char *op))(stack_t **stack, unsigned int line_number);
-void _push_(stack_t **head, unsigned int c_line);
-void _pall_(stack_t **head, unsigned int c_line);
-void _pint_(stack_t **head, unsigned int c_line);
-void _pop_(stack_t **head, unsigned int c_line);
-void _swap_(stack_t **head, unsigned int c_line);
-void _queue_(stack_t **head, unsigned int c_line);
-void _stack_(stack_t **head, unsigned int c_line);
-void _add_(stack_t **head, unsigned int c_line);
-void _nop_(stack_t **head, unsigned int c_line);
-void _sub_(stack_t **head, unsigned int c_line);
-void _rotl_(stack_t **head, unsigned int c_line);
-void _rotr_(stack_t **head, unsigned int c_line);
-int _strcmp_(char *str1, char *str2);
-int _sch_(char *str, char c);
-char *_strtoky_(char *s, char *c);
-void free_vglo(void);
+char *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);
+ssize_t getstdin(char **lineptr, int file);
+char  *clean_line(char *content);
+void _push(stack_t **head, unsigned int number);
+void _pall(stack_t **head, unsigned int number);
+void _pint(stack_t **head, unsigned int number);
+int execute(char *content, stack_t **head, unsigned int counter, FILE *file);
+void free_stack(stack_t *head);
+void _pop(stack_t **head, unsigned int counter);
+void _swap(stack_t **head, unsigned int counter);
+void _add(stack_t **head, unsigned int counter);
+void _nop(stack_t **head, unsigned int counter);
+void _sub(stack_t **head, unsigned int counter);
+void _div(stack_t **head, unsigned int counter);
+void _mul(stack_t **head, unsigned int counter);
+void _mod(stack_t **head, unsigned int counter);
+void _pchar(stack_t **head, unsigned int counter);
+void _pstr(stack_t **head, unsigned int counter);
+void _rotl(stack_t **head, unsigned int counter);
+void _rotr(stack_t **head, __attribute__((unused)) unsigned int counter);
+void addnode(stack_t **head, int n);
+void addqueue(stack_t **head, int n);
+void _queue(stack_t **head, unsigned int counter);
+void _stack(stack_t **head, unsigned int counter);
 
 #endif /*MONTY_H*/
